798564dfb9df50cebf94c2aa3716e2aab7df5753
spartanGhost17 <adambaku5216@gmail.com>
2024-08-15 21:30:06 +0100

    added securityweb filter chain for gateway

--- services/gateway/src/main/java/com/ecommerce/gateway/config/security/SecurityConfig.java
+++ services/gateway/src/main/java/com/ecommerce/gateway/config/security/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.ecommerce.gateway.config.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Bean("Security Web FilterChain")
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange ->
+                        exchange
+                                .pathMatchers("/eureka/**").permitAll()
+                                .anyExchange()
+                                .authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
 }
